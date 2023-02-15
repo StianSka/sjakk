0ce778c7ccba3b0553692b8f5a77b965c6704fc5
StianSka <116058912+StianSka@users.noreply.github.com>
2023-02-15 19:03:02 +0100

    sjakk

--- pawnController.js
+++ pawnController.js
@@ -15,31 +15,41 @@ function getWhitePawnMoves(currentPosIndex) {
 
 function whitePawnDoubleMowe(currentPosIndex) {
     let hasHitPiece = false
-    if (checkForPiece(currentPosIndex + 1) == true) { hasHitPiece = true }
-    else { model.board[(currentPosIndex + 1)].color = model.legalMoveSquareColor }
-    if (checkForPiece(currentPosIndex + 2) == false && hasHitPiece == false) {
-        model.board[(currentPosIndex + 2)].color = model.legalMoveSquareColor
+    if (currentPosIndex + 1 <= model.board.length) {
+        if (checkForPiece(currentPosIndex + 1) == true) { hasHitPiece = true }
+        else { model.board[(currentPosIndex + 1)].color = model.legalMoveSquareColor }
+    }
+    if (currentPosIndex + 2 <= model.board.length) {
+        if (checkForPiece(currentPosIndex + 2) == false && hasHitPiece == false) {
+            model.board[(currentPosIndex + 2)].color = model.legalMoveSquareColor
+        }
     }
 }
 
 function whitePawnSingleMowe(currentPosIndex) {
-    if (checkForPiece(currentPosIndex + 1) == false) {
-        model.board[(currentPosIndex + 1)].color = model.legalMoveSquareColor
+    if (currentPosIndex + 1 <= model.board.length) {
+        if (checkForPiece(currentPosIndex + 1) == false) {
+            model.board[(currentPosIndex + 1)].color = model.legalMoveSquareColor
+        }
     }
 }
 
 function whitePawnCapture(currentPosIndex) {
-    if (checkForPiece(currentPosIndex - 7) == true &&
-        getPieceCollor(currentPosIndex - 7) != model.inputs.currentlyMovingPiece.color) {
-        model.board[(currentPosIndex - 7)].color = model.legalMoveSquareColor
+    if (currentPosIndex - 7 >= 0) {
+        if (checkForPiece(currentPosIndex - 7) == true &&
+            getPieceCollor(currentPosIndex - 7) != model.inputs.currentlyMovingPiece.color) {
+            model.board[(currentPosIndex - 7)].color = model.legalMoveSquareColor
+        }
     }
-    if (checkForPiece(currentPosIndex + 9) == true &&
-        getPieceCollor(currentPosIndex + 9) != model.inputs.currentlyMovingPiece.color) {
-        model.board[(currentPosIndex + 9)].color = model.legalMoveSquareColor
+    if (currentPosIndex + 9 <= model.board.length) {
+        if (checkForPiece(currentPosIndex + 9) == true &&
+            getPieceCollor(currentPosIndex + 9) != model.inputs.currentlyMovingPiece.color) {
+            model.board[(currentPosIndex + 9)].color = model.legalMoveSquareColor
+        }
     }
 }
 
-function getBlackPawnMoves(currentPosIndex){
+function getBlackPawnMoves(currentPosIndex) {
     blackPawnCapture(currentPosIndex)
     if (model.inputs.currentlyMovingPiece.hasMoved == false) { blackPawnDoubleMowe(currentPosIndex) }
     else { blackPawnSingleMowe(currentPosIndex) }
@@ -47,26 +57,36 @@ function getBlackPawnMoves(currentPosIndex){
 
 function blackPawnDoubleMowe(currentPosIndex) {
     let hasHitPiece = false
-    if (checkForPiece(currentPosIndex - 1) == true) { hasHitPiece = true }
-    else { model.board[(currentPosIndex - 1)].color = model.legalMoveSquareColor }
-    if (checkForPiece(currentPosIndex - 2) == false && hasHitPiece == false) {
-        model.board[(currentPosIndex - 2)].color = model.legalMoveSquareColor
+    if (currentPosIndex - 1 >= 0) {
+        if (checkForPiece(currentPosIndex - 1) == true) { hasHitPiece = true }
+        else { model.board[(currentPosIndex - 1)].color = model.legalMoveSquareColor }
+    }
+    if (currentPosIndex - 2 >= 0) {
+        if (checkForPiece(currentPosIndex - 2) == false && hasHitPiece == false) {
+            model.board[(currentPosIndex - 2)].color = model.legalMoveSquareColor
+        }
     }
 }
 
 function blackPawnSingleMowe(currentPosIndex) {
-    if (checkForPiece(currentPosIndex - 1) == false) {
-        model.board[(currentPosIndex - 1)].color = model.legalMoveSquareColor
+    if (currentPosIndex - 1 >= 0) {
+        if (checkForPiece(currentPosIndex - 1) == false) {
+            model.board[(currentPosIndex - 1)].color = model.legalMoveSquareColor
+        }
     }
 }
 
 function blackPawnCapture(currentPosIndex) {
-    if (checkForPiece(currentPosIndex - 9) == true &&
-        getPieceCollor(currentPosIndex - 9) != model.inputs.currentlyMovingPiece.color) {
-        model.board[(currentPosIndex - 9)].color = model.legalMoveSquareColor
+    if (currentPosIndex - 9 >= 0) {
+        if (checkForPiece(currentPosIndex - 9) == true &&
+            getPieceCollor(currentPosIndex - 9) != model.inputs.currentlyMovingPiece.color) {
+            model.board[(currentPosIndex - 9)].color = model.legalMoveSquareColor
+        }
     }
-    if (checkForPiece(currentPosIndex + 7) == true &&
-        getPieceCollor(currentPosIndex + 7) != model.inputs.currentlyMovingPiece.color) {
-        model.board[(currentPosIndex + 7)].color = model.legalMoveSquareColor
+    if (currentPosIndex + 7 <= model.board.length) {
+        if (checkForPiece(currentPosIndex + 7) == true &&
+            getPieceCollor(currentPosIndex + 7) != model.inputs.currentlyMovingPiece.color) {
+            model.board[(currentPosIndex + 7)].color = model.legalMoveSquareColor
+        }
     }
 }
